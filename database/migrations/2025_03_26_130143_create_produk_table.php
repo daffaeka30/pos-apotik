010b7914961b57ca99cf103b382e0d3570941a24
daffaeka30 <daffaekaputri30@gmail.com>
2025-03-26 20:26:02 +0700

    Migrasi Database

---
+++ database/migrations/2025_03_26_130143_create_produk_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->increments('id_produk');
+            $table->integer('id_kategori');
+            $table->string('nama_produk')->unique();
+            $table->string('merk')->nullable();
+            $table->integer('harga_beli');
+            $table->tinyInteger('diskon');
+            $table->integer('harga_jual');
+            $table->integer('stok');
+            $table->string('satuan'); // Tablet, Botol, Strip, dll.
+            $table->date('expired');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produk');
+    }
+};
