329da80eb4cec916a9346b58300901d92de5fc9c
daffaeka30 <daffaekaputri30@gmail.com>
2025-05-06 19:30:10 +0700

    Footer Update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,6 +2,6 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> 2.4.18
     </div>
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
+    <strong>Copyright &copy; 2025 <a href="#">{{ $setting->nama_perusahaan }}</a>.</strong> All rights
     reserved.
 </footer>
